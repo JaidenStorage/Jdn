c25674940b8fc67fe3d894995c534a2645d7462f
JaidenStorage <digier@naver.com>
2021-01-13 23:10:11 +0900

    문자열 파트 수정

--- Basic_C++/Basic_C++.cpp
+++ Basic_C++/Basic_C++.cpp
@@ -8,51 +8,42 @@ void _Bin_Oct_Dec_Hex()
 	unsigned char Hex = 0x55;			// 16진법 표기법
 }
 
-
-void _string()
+void _text()
 {
-	// #include <string>
+	// char		: 1byte
+	// w_char	: 2byte
+	// t_char	: multi-byte일때 char, Unicode일때 w_char
 
-	string_ = std::to_string(int_);		// int -> string
-	string_ = std::to_string(float_);	// float -> string
+	// LPSTR	= long pointer string				= char *
+	// LPCSTR	= long pointer constant stsring		= const char *
+	// LPWSTR	= long pointer wide string			= w_char*
+	// LPCWSTR	= long pointer constant wide string = const w_char*
+	// LPCTSTR	= long pointer constant t_string	= const t_char *
 
-	int_ = atoi(string_.c_str());		// string -> int
-	float_ = atof(string_.c_str());		// string -> float
-}
-
-
-void _CString()
-{
-	//#include <afxstr.h>
-
-	int_ = _ttoi(CString_);				// CString -> int
-	float_ = _ttof(CString_);			// CString -> float
-
-	CString_.Format(_T("%02d     %0.2f     "), int_, float_);
-	CString_.Delete(0, CString_.GetLength());
-		 // .GetLength			설정된 문자열의 길이를 리턴
-		 // .IsEmpty			문자열 버퍼가 비워져 있으면 TRUE, 그렇지 않으면 FALSE
-		 // .Empty				문자열을삭제하여 버퍼를 비움
-		 // .GetAt				문자열의 특정 위치의 문자값을 얻음
-		 // .SetAt				문자열의 특정 위치에 새로운 문자를 삽입
-		 // .Compare			문자열을 비교, 같으면 0, 그렇지 않으면 0 이 아닌 값을 리턴
-		 // .Mid, Left, Right	문자열 중간, 좌측, 우측부터 특정 위치 문자열을 CString 으로 추출
-		 // .MakeUpper			문자열을 모두 대문자로 바꿈
-		 // .MakeLower			문자열을 모두 소문자로 바꿈
-		 // .Format				sprintf 형태로 문자열에 문자 넣기
-		 // .Find				문자열에서 특정 문자(열)를 찾기
+	// char		char_[]		= "Hello World!";
+	// wchar_t	wchar_t_[]	= L"Hello World!";
+	// TCHAR	TCHAR_[]	= _T("Hello World!");
 }
 
 
 void _Char()
 {
-	sprintf_s(char__, sizeof(char__), "%d", int_);	// int -> char*
-	sprintf_s(char__, sizeof(char__), "%f", float_);	// float -> char*
+	// Number <-> Text
+	{
+		sprintf_s(pchar_, sizeof(pchar_), "%d", int_);		// int -> char*
+		sprintf_s(pchar_, sizeof(pchar_), "%f", float_);	// float -> char*
 
-	int_ = atoi(char__);		// char* -> int
-	double_ = atof(char__);		// char* -> float
-	long_ = atol(char__);		// char* -> long
+		int_ = atoi(pchar_);		// char* -> int
+		double_ = atof(pchar_);		// char* -> float
+	}
 
+	// Text <-> Text
+	{
+		CString_ = pchar_;						// char* -> CString
+
+		string_ = pchar_;						// char* -> string
+		cchar_ = string_.c_str();				// string -> const char*
+	}
 
 	size_t strlen(const char* s);		// 문자열 길이
 	char* strcpy(char* dest, const char* src);				// 문자열 복사
@@ -62,6 +53,78 @@ void _Char()
 	int strcmp(const char* s1, const char* s2);				// 문자열 비교함수, 같으면 0 반환
 	int strncmp(const char* s1, const char* s2, size_t n);	// 문자열 비교함수, 같으면 0 반환
 }
+
+void _string()
+{
+	// #include <string>
+
+	std::string A("Hello");
+	std::string B = A + "!";
+
+	// Number <-> Text
+	{
+		// int
+		string_ = std::to_string(int_);		// int -> string
+		int_ = atoi(string_.c_str());		// string -> int
+		
+		//float
+		string_ = std::to_string(float_);	// float -> string
+		float_ = atof(string_.c_str());		// string -> float
+	}
+
+	// Text <-> Text
+	{
+		// char
+		cchar_ = string_.c_str();				// string -> const char*
+		string_ = pchar_;						// char* -> string
+
+		// CString
+		CString_ = string_.c_str();				// string -> CString
+		string_ = CT2CA(CString_);				// CString -> string
+	}
+}
+
+
+void _CString()
+{
+	//#include <afxstr.h>
+	
+	CString_.Delete(0, CString_.GetLength());
+		//  .GetLength			설정된 문자열의 길이를 리턴
+		//  .IsEmpty			문자열 버퍼가 비워져 있으면 TRUE, 그렇지 않으면 FALSE
+		//  .Empty				문자열을삭제하여 버퍼를 비움
+		//  .GetAt				문자열의 특정 위치의 문자값을 얻음
+		//  .SetAt				문자열의 특정 위치에 새로운 문자를 삽입
+		//  .Compare			문자열을 비교, 같으면 0, 그렇지 않으면 0 이 아닌 값을 리턴
+		//  .Mid, Left, Right	문자열 중간, 좌측, 우측부터 특정 위치 문자열을 CString 으로 추출
+		//  .MakeUpper			문자열을 모두 대문자로 바꿈
+		//  .MakeLower			문자열을 모두 소문자로 바꿈
+		//  .Format				sprintf 형태로 문자열에 문자 넣기
+		//  .Find				문자열에서 특정 문자(열)를 찾기
+
+	// Number <-> Text
+	{
+		// int
+		int_ = _ttoi(CString_);						// CString -> int
+		CString_.Format(_T("%02d"),int_);			// int -> CString
+
+		// float
+		float_ = _ttof(CString_);					// CString -> float
+		CString_.Format(_T("%02f"), float_);		// float -> CString
+	}
+	
+	// Text <-> Text
+	{
+		// TCHAR
+		pTCHAR_ = (TCHAR*)(LPCTSTR)CString_;			// CString -> TCHAR
+		CString_ = (LPCTSTR)pTCHAR_;					// TCHAR -> CString
+
+		// char
+		CString_ = cchar_;							// char* -> CString
+	}
+}
+
+
 
 
 void _vector()
--- Basic_C++/Basic_C++.h
+++ Basic_C++/Basic_C++.h
@@ -1,11 +1,14 @@
 #include <afxstr.h>
 #include <string>
 #include <vector>
+#include <tchar.h>
 
 bool bool_;
 
 char char_;
-char* char__;
+char* pchar_;
+const char* cchar_;
+
 int int_;
 long long_;
 float float_;
@@ -13,3 +16,4 @@ double double_;
 
 std::string string_;	
 CString CString_;	
+TCHAR* pTCHAR_;
